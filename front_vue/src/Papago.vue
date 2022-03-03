f4ae190a290756d83f1d1169c7584a86a07d5b7d
monoha724 <bunnyalex724@gmail.com>
2022-03-03 15:35:26 +0900

    papago wireframe

---
+++ front_vue/src/Papago.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <div>
+          <h3>간단번역</h3>
+          <button type="button" id="loginBtn" class="loginBtn"><img src="./assets/weblogin1.png" width="50" id="loginImage" ></button>
+      </div>
+      <div>
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
