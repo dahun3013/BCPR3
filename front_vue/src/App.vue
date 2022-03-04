8c521a8cc8095f871476077b3dc128d1b6f64f70
monoha724 <bunnyalex724@gmail.com>
2022-03-04 09:43:23 +0900

    second commit

--- front_vue/src/App.vue
+++ front_vue/src/App.vue
@@ -3,18 +3,18 @@
   <div id="mainBox"> <!-- 전체 -->
         <div id="headerArea"> <!-- 상단 blue -->
             <div id="loginArea"> <!-- 로그인 버튼 -->
-                <button type="button" id="loginBtn" class="loginBtn"><img src="./assets/weblogin1.png" width="50" id="loginImage" ></button>
+                <router-link to="#"><button type="button" id="loginBtn" class="loginBtn"><img src="@/assets/weblogin1.png" width="50" id="loginImage" ></button></router-link>
             </div>
             <br><br><br><br>
             <h1>PAGO BOOKS</h1>
-            <h5>로그인을 하시면 저장 기능을 사용하실 수 있습니다.</h5>
+            <h4>로그인을 하시면 저장 기능을 사용하실 수 있습니다.</h4>
             
             <div class="modal">
-                <img src="./assets/cancleBtn.png" class="cancleBtn">
+                <img src="@/assets/cancleBtn.png" class="cancleBtn">
                 <div id="loginBtns">
-                <button type="button" id="naverBtn"><img src="./assets/naverLogo.png" width="35px"> 네이버 아이디로 로그인</button>
-                <button type="button" id="googleBtn"><img src="./assets/googleLogo.png" width="40px"> 구글 아이디로 로그인</button>
-                <button type="button" id="githubBtn"><img src="./assets/githubLogo.png" width="45px"> Github 아이디로 로그인</button>
+                <button type="button" id="naverBtn"><img src="@/assets/naverLogo.png" width="35px"> 네이버 아이디로 로그인</button>
+                <button type="button" id="googleBtn"><img src="@/assets/googleLogo.png" width="40px"> 구글 아이디로 로그인</button>
+                <button type="button" id="githubBtn"><img src="@/assets/githubLogo.png" width="45px"> Github 아이디로 로그인</button>
             </div>
                 <h3>아이디/비밀번호 찾기</h3>
                 
@@ -22,13 +22,13 @@
 
         </div>
           <div id="images">
-            <img src="./assets/1.png" id="firstImage">
-            <img src="./assets/2.png" id="secondImage">
-            <img src="./assets/3.png" id="thirdImage">
-            <router-link to="/SimpleTranse.vue"><img src="./assets/4.png" id="fourthImage"></router-link>
+            <img src="@/assets/1.png" id="firstImage">
+            <img src="@/assets/2.png" id="secondImage">
+            <img src="@/assets/3.png" id="thirdImage">
+            <router-link to="/papago"><img src="@/assets/4.png" id="fourthImage"></router-link>
           </div>
         <div id="bottomBox"> <!-- 하단 white -->
-            <img src="source/사각형 2.png" id="bottomWhite">
+            <img src="@/assets/사각형2.png" id="bottomWhite">
         </div>
     </div>
 
