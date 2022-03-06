c37c82500cb3ecb81ca5bddb2c9e1b84cc03c80c
monoha724 <bunnyalex724@gmail.com>
2022-03-07 00:43:54 +0900

    add web pages(70%...?)

---
+++ front_vue2/src/components/Modal.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login-modal px-5 py-5" v-if="loginModal == true">
+      <h4>로그인</h4>
+      <div class="loginBtns">
+        <div>
+          <img src="@/assets/naverLogo.png" alt=""> 네이버 아이디로 로그인
+        </div>
+        <div>
+          <img src="@/assets/kakaoLogo.png" alt="">카카오 아이디로 로그인
+        </div>
+        <div>
+          <img src="@/assets/googleLogo.png" alt="">구글 아이디로 로그인
+        </div>
+        <div>
+          <img src="@/assets/githubLogo.png" alt="">깃허브 아이디로 로그인
+        </div>
+      </div>
+      <div>
+        <button @click="$emit('closeModal')">닫기</button>
+      </div>
+    </div><!--login-modal-end-->
+</template>
+
+<script>
+export default {
+    name: 'ModalPopup',
+    data() {
+      return {
+        
+      }
+    },
+    props : {
+      modal : Boolean,
+    }
+}
+</script>
+
+<style>
+
+</style>
