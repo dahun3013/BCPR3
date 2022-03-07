92a046a7782509e908be618f28d706c239d29f7b
monoha724 <bunnyalex724@gmail.com>
2022-03-07 12:10:55 +0900

    LoginModal can use/Design not yet

--- front_vue2/src/pages/OCR.vue
+++ front_vue2/src/pages/OCR.vue
@@ -69,6 +69,28 @@
         <p class="mx-3">책임의 한계와 법적고지</p>
         <p class="mx-3">준수사항</p>
     </div>
+
+    <div class="login-modal px-5 py-5" v-if="loginModal == true">
+        <h4>로그인</h4>
+        <div class="loginBtns">
+            <div>
+                <img src="@/assets/naverLogo.png" alt=""> 네이버 아이디로 로그인
+            </div>
+            <div>
+                <img src="@/assets/kakaoLogo.png" alt="">카카오 아이디로 로그인
+            </div>
+            <div>
+                <img src="@/assets/googleLogo.png" alt="">구글 아이디로 로그인
+            </div>
+            <div>
+                <img src="@/assets/githubLogo.png" alt="">깃허브 아이디로 로그인
+            </div>
+        </div>
+        <div>
+            <button @click="loginModal = false">닫기</button>
+        </div>
+    </div><!--login-modal-end-->
+
 </template>
 
 <script>
@@ -79,7 +101,8 @@ export default {
     name: 'papagoPage',
     data() {
         return {
-            image : ''
+            image : '',
+            loginModal : false,
         }
     },
     components : {
