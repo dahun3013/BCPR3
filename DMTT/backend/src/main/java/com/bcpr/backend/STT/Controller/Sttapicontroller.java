d995e6e4ed93c57492c335c5f078b00f0cb62e8e
HelloWorld김희태 <93626542+Kimhuitae@users.noreply.github.com>
2022-03-07 13:47:34 +0900

    Update Sttapicontroller.java
    뷰에서 파람값

--- DMTT/backend/src/main/java/com/bcpr/backend/STT/Controller/Sttapicontroller.java
+++ DMTT/backend/src/main/java/com/bcpr/backend/STT/Controller/Sttapicontroller.java
@@ -14,6 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @RestController
+@RequestMapping("/api")
+@CrossOrigin
 public class Sttapicontroller {
     private SttMapper mapper;
 
@@ -21,17 +23,38 @@ public class Sttapicontroller {
     public void setMapper(SttMapper mapper) {
         this.mapper = mapper;
     }
-    @PostMapping("/Stt/Kor")
+    @PostMapping("/Stt/")
     public String getKorStt(@RequestParam(value="file", required=false) MultipartFile file,
+                            @RequestParam(value="lang") String lang,
                             HttpServletRequest request) throws IOException{
         FileSaveHelper fsh=new FileSaveHelper(request.getServletContext().getRealPath("resources"));
-
+        System.out.println("lang"+ lang);
         String url= fsh.tempSave(file);
-        SttKorHelper SKH=new SttKorHelper();
-        String out= SKH.Sttkor(url);
+        SttHelper SH=new SttHelper();
+        String out= SH.getSTThelper(url,lang);
         System.out.println("out :"+ out);
         fsh.delete(url);
 
         return out;
+    }
+
+    @PostMapping("/Stt/upload")
+    public int upload(
+            @RequestParam("email") String email,
+            @RequestParam("trans_date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime trans_date,
+            @RequestParam("kind") String kind,
+            @RequestParam(value="input", required=false) MultipartFile input,
+            @RequestParam("output") String output,
+            HttpServletRequest request)throws IOException {
+
+        FileSaveHelper fsh = new FileSaveHelper(request.getServletContext().getRealPath("resources"));
+        String url = fsh.document_transSave(email, kind, trans_date, input);
+        return mapper.insertMedia_TransContent(email,trans_date,kind,url,output);
+    }
+    @GetMapping("/Stt/download")
+    public List<Document_Trans> download(
+            @RequestParam("email") String email){
+        //log.info("test : {}",test.get(0));
+        return mapper.getDocument_Trans(email);
     }
 }
