e73b2250546e0be8c1e2b252728cb27a0b6b8235
HelloWorld김희태 <93626542+Kimhuitae@users.noreply.github.com>
2022-03-14 15:47:36 +0900

    Stt upload download 매핑 확인필요

--- DMTT/backend/src/main/java/com/bcpr/backend/STT/Controller/Sttapicontroller.java
+++ DMTT/backend/src/main/java/com/bcpr/backend/STT/Controller/Sttapicontroller.java
@@ -1,13 +1,10 @@
 package com.bcpr.backend.STT.Controller;
 
+import com.bcpr.backend.ocr.domain.Media_Trans;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.bcpr.backend.STT.Helper.SttHelper;
@@ -16,7 +13,12 @@ import com.bcpr.backend.STT.domain.Document_Trans;
 import com.bcpr.backend.STT.util.FileSaveHelper;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -55,12 +57,61 @@ public class Sttapicontroller {
 
         FileSaveHelper fsh = new FileSaveHelper(request.getServletContext().getRealPath("resources"));
         String url = fsh.document_transSave(email, kind, trans_date, input);
-        return mapper.insertMedia_TransContent(email,trans_date,kind,url,output);
+        return mapper.insertDocument_TransContent(email,trans_date,kind,url,output);
     }
-    @GetMapping("/Stt/download")
-    public List<Document_Trans> download(
-            @RequestParam("email") String email){
+    @GetMapping("/Stt/list/{email}")
+    public List<Document_Trans> getList(
+            @PathVariable("email") String email){
         //log.info("test : {}",test.get(0));
-        return mapper.getDocument_Trans(email);
+        return mapper.getDocument_TransListByEmail(email);
+    }
+
+    @GetMapping("/Stt/download/{email}/{document_no}/{kind}")
+    public void download(
+            @PathVariable("email") String email,
+            @PathVariable("document_no") int document_no,
+            @PathVariable("kind") String kind,
+            HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        Media_Trans mt = mapper.getDocument_Trans(email, document_no);
+
+        String path = "";
+        path = request.getServletContext().getRealPath("resources")
+                +"\\document_no\\"
+                + mt.getEmail()
+                + "\\";
+
+        if(kind.equals("input")) {
+            path += mt.getInput(); // 경로에 접근할 때 역슬래시('\') 사용
+
+        }else {
+            path += mt.getTrans_date()+"-output.txt";
+            try{
+                // BufferedWriter 와 FileWriter를 조합하여 사용 (속도 향상)
+                BufferedWriter fw = new BufferedWriter(new FileWriter(path, true));
+                // 파일안에 문자열 쓰기
+                fw.write(mt.getOutput());
+                fw.flush();
+                // 객체 닫기
+                fw.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        File file = new File(path);
+        byte[] fileByte = FileUtils.readFileToByteArray(file);
+
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment; fileName=\"" + URLEncoder.encode(file.getName(), "UTF-8")+"\"");
+        response.setHeader("Content-Transfer-Encoding", "binary");
+
+        response.getOutputStream().write(fileByte);
+        response.getOutputStream().flush();
+        response.getOutputStream().close();
+
+        if(kind.equals("output") && file.exists() ){
+            if(file.delete()){
+            }
+        }
     }
 }
