0ca9f9e4501b100008ac377dbfc0b71fc83f9a9f
monoha724 <bunnyalex724@gmail.com>
2022-03-15 20:23:16 +0900

    보관함 안내 디자인 완료

--- front_vue2/src/pages/Storage.vue
+++ front_vue2/src/pages/Storage.vue
@@ -20,7 +20,20 @@
     </div>
     <!--storage-bottom-container-end-->
   </div>
-  <div class="storage-info px-5">보관함 안내 박스 테스트</div>
+  <div class="storage-info-cont">
+    <div class="storage-info mx-5 my-3 px-5">
+        <img src="@/assets/step1.png" alt="">
+        <h6>로그인을 하시면 파일 저장과 보관함 이용이 가능합니다</h6>
+    </div>
+    <div class="storage-info mx-5 my-3 px-5">
+        <img src="@/assets/step2.png" alt="">
+        <h6>파일을 저장을 했다면, 상단 메뉴에서 보관함을 선택하세요</h6>
+    </div>
+    <div class="storage-info mx-5 my-3 px-5">
+        <img src="@/assets/step3.png" alt="">
+        <h6>저장한 파일을 확인하고 다운 받아 사용하세요</h6>
+    </div>
+  </div>
   <!--field_end-->
   <Modal @closeModal="loginModal = false" :loginModal="loginModal" />
 </template>
@@ -56,7 +69,7 @@ textarea {
 }
 
 .title-name {
-  margin: auto;
+  margin-left: 43%;
 }
 
 .profile-logo > div > h2 {
@@ -95,5 +108,30 @@ select:hover {
 
 select:focus {
   outline: none;
+}
+
+.storage-info-cont {
+  display: flex;
+  margin-bottom: 5.4rem;
+}
+
+.storage-info {
+  padding: 3rem;
+  text-align: center;
+  width: 100%;
+  border: 1px solid #dbdbdb;
+  border-radius: 10px;
+}
+
+.storage-info > img{
+  width: 100%;
+}
+
+.storage-info > h3 {
+  color: #767676;
+}
+
+.storage-info > h6 {
+  margin-top: 3rem;
 }
 </style>
