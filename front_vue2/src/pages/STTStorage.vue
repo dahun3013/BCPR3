c7bf6c40b4f7ae0d06be9f7e537c45f6b5499030
dahun3013 <dahun3013@gmail.com>
2022-03-15 14:10:32 +0900

    수정

--- front_vue2/src/pages/STTStorage.vue
+++ front_vue2/src/pages/STTStorage.vue
@@ -3,7 +3,7 @@
     <div class="top-container px-5 py-5">
       <div class="profile-logo">
         <div style="text-align: left">
-          <h2>매체변환 보관함</h2>
+          <h2>음성변환 보관함</h2>
         </div>
         <div class="title-name">
           <h2 @click="$router.push('/')">PAGO BOOKS</h2>
@@ -14,7 +14,7 @@
     <!--top-container-end-->
 
     <div class="storage-bottom-container px-5 pt-5 pb-2 justify-content-center">
-      <SSM :state="3" />
+      <SSM :state="2" />
     </div>
     <!--storage-bottom-container-end-->
   </div>
@@ -182,7 +182,6 @@ select:focus {
   border-radius: 10px;
   object-fit: contain;
   display: block;
-  
 }
 
 .storage-ff1-btn {
