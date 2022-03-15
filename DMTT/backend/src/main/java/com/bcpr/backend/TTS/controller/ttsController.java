7e539b57c530b4c6bdef38cbd2f457e10a2e58ac
JangG1 <jangjiwon0417@naver.com>
2022-03-15 13:39:59 +0900

    TTS

--- DMTT/backend/src/main/java/com/bcpr/backend/TTS/controller/ttsController.java
+++ DMTT/backend/src/main/java/com/bcpr/backend/TTS/controller/ttsController.java
@@ -112,7 +112,7 @@ public class ttsController {
 	    response.getOutputStream().close();
 	    
 	    if(kind.equals("input")){ 
-			fsh.deleteFile(file);
+		fsh.deleteFile(file);
 		}
     }
 	
---
+++ front_vue2/src/components/TTSStorageCard.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="storage-ts-container mb-5">
+    <div class="storage-ts-output-cont">
+      <div style="float: left; width: 20%">
+        <h2>매체변환</h2>
+      </div>
+      <div style="float: left; width: 75%">
+        <h3>{{ trans_date }}</h3>
+      </div>
+      <div style="float: right; width: 5%">
+        <button @click="remove()">삭제</button>
+      </div>
+      <div class="storage-img-box">
+        <video class="img-fit" controls ref="player" v-show="showInput">
+          <source :src="input" />
+        </video>
+        <div style="display: none">
+          <audio class="img-fit" controls ref="player">
+            <source :src="input" />
+          </audio>
+        </div>
+
+        <output name="result" v-show="showOutput">
+          <div v-html="content"></div>
+        </output>
+      </div>
+      <div style="display: flex">
+        <button @click="changeShow()" class="storage-ff1-btn mt-4">
+          텍스트 보기
+        </button>
+        <button @click="download()" class="storage-ff2-btn mt-4">
+          다운로드
+        </button>
+      </div>
+    </div>
+    <!--storage-ts-output-cont-end-->
+  </div>
+  <!--storage-ts-container-end-->
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "TTSStorageCard",
+  props: {
+    voice_no: Number,
+    email: String,
+    kind: String,
+    input: String,
+    output: String,
+    trans_date: String,
+  },
+  data() {
+    return {
+      showInput: true,
+      showOutput: false,
+    };
+  },
+  methods: {
+    changeShow() {
+      if (this.showInput) this.showInput = false;
+      else this.showInput = true;
+      if (this.showOutput) this.showOutput = false;
+      else this.showOutput = true;
+    },
+    async remove() {
+      let str = "/api/tts/remove";
+      let form = new FormData();
+      form.append("email", this.email);
+      form.append("voice_no", this.voice_no);
+
+      await axios
+        .post(str, form)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      this.$router.go("/storage");
+    },
+    download() {
+      let str =
+        "/api/tts/download/" + this.email + "/" + this.voice_no + "/";
+      if (this.showInput) str += "input";
+      else str += "output";
+
+      axios
+        .get(str, {
+          responseType: "blob",
+        })
+        .then((res) => {
+          const name = res.headers["content-disposition"]
+            .split("fileName=")[1]
+            .replace(/"/g, "");
+          const url = window.URL.createObjectURL(new Blob([res.data]));
+          const link = document.createElement("a");
+          link.href = url;
+          link.setAttribute("download", name); //or any other extension document.body.appendChild(link); link.click();
+          document.body.appendChild(link);
+          link.click();
+          link.remove();
+          console.log(res);
+          console.log("다운로드 성공");
+        })
+        .catch((err) => {
+          err;
+          console.log("다운로드 실패");
+        });
+    },
+  },
+  computed: {
+    content() {
+      return this.output.replace(/(?:\r\n|\r|\n)/g, "<br />");
+    },
+  },
+};
+</script>
+
+<style>
+.player {
+  width: 100%;
+}
+</style>
